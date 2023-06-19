981e7454f3b3f8ad339fd34eb98089b51a97524c
mathieu <mathieu.pinaud@laplateofrme.io>
2023-06-19 18:11:40 +0200

    front ok

--- src/main/java/front/calculater/FrontController.java
+++ src/main/java/front/calculater/FrontController.java
@@ -4,11 +4,113 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class FrontController {
-    @FXML
-    private Label welcomeText;
+    public Label litExp;
+    public String txt = "";
+    public char last_input;
+    public void button0Clic() {
+        this.txt += "0";
+        litExp.setText(txt);
+        this.last_input = '0';
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void button1Clic() {
+        this.txt += "1";
+        litExp.setText(txt);
+        this.last_input = '1';
+    }
+
+    public void button2Clic() {
+        this.txt += "2";
+        litExp.setText(txt);
+        this.last_input = '2';
+    }
+
+    public void button3Clic() {
+        this.txt += "3";
+        litExp.setText(txt);
+        this.last_input = '3';
+    }
+
+    public void button4Clic() {
+        this.txt += "4";
+        litExp.setText(txt);
+        this.last_input = '4';
+    }
+
+    public void button5Clic() {
+        this.txt += "5";
+        litExp.setText(txt);
+        this.last_input = '5';
+    }
+
+    public void button6Clic() {
+        this.txt += "6";
+        litExp.setText(txt);
+        this.last_input = '6';
+    }
+
+    public void button7Clic() {
+        this.txt += "7";
+        litExp.setText(txt);
+        this.last_input = '7';
+    }
+
+    public void button8Clic() {
+        this.txt += "8";
+        litExp.setText(txt);
+        this.last_input = '8';
+    }
+
+    public void button9Clic() {
+        this.txt += "9";
+        litExp.setText(txt);
+        this.last_input = '9';
+    }
+
+    public void buttonDotClic() {
+        this.txt += ".";
+        litExp.setText(txt);
+        this.last_input = '.';
+    }
+
+    public void buttonPlusClic() {
+        this.txt += " + ";
+        litExp.setText(txt);
+        this.last_input = '+';
+    }
+
+    public void buttonMinusClic() {
+        this.txt += " - ";
+        litExp.setText(txt);
+        this.last_input = '-';
+    }
+
+    public void buttonMultipleClic() {
+        this.txt += " * ";
+        litExp.setText(txt);
+        this.last_input = '*';
+    }
+
+    public void buttonDivideClic() {
+        this.txt += " / ";
+        litExp.setText(txt);
+        this.last_input = '/';
+    }
+
+    public void buttonSuppClic() {
+        if (this.txt.length() != 0) {
+            if (this.txt.charAt(this.txt.length() - 1) == ' ')
+                this.txt = this.txt.substring(0, this.txt.length() - 3);
+            else
+                this.txt = this.txt.substring(0, this.txt.length() - 1);
+            litExp.setText(txt);
+        }
+        this.last_input = 'X';
+    }
+
+    public void buttonCleanClic() {
+        this.txt = "";
+        litExp.setText(txt);
+        this.last_input = 'C';
     }
 }
--- src/main/java/front/calculater/Launcher.java
+++ src/main/java/front/calculater/Launcher.java
@@ -11,7 +11,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("classic-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Calc-u-later!");
         stage.setScene(scene);
         stage.show();
